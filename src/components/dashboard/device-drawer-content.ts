df096b0751140456a58d45205ac09f2c9b204a14
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-05-06 15:35:28 +0200

    Show device Area in drawer and as opt-in table column

--- src/components/dashboard/device-drawer-content.ts
+++ src/components/dashboard/device-drawer-content.ts
@@ -544,6 +544,7 @@ export class ESPHomeDeviceDrawerContent extends LitElement {
         ${this._renderBluetoothMacRow(d)}
         ${this._row("memory", this._localize("dashboard.drawer_platform"), d.target_platform)}
         ${this._renderBuildSizeRow(d)}
+        ${d.area ? this._row("map-marker-outline", this._localize("dashboard.drawer_area"), d.area) : nothing}
       </div>
 
       ${this._renderReachabilitySection()}
