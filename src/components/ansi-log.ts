92bac85bdf6f1d00b4f0785dd7192c66bb8ed974
J. Nick Koston <nick@home-assistant.io>
2026-06-01 14:40:40 -0500

    Tighten the log surface padding

--- src/components/ansi-log.ts
+++ src/components/ansi-log.ts
@@ -292,7 +292,7 @@ export class ESPHomeAnsiLog extends LitElement {
           Consolas, monospace;
         font-variant-ligatures: none;
         font-size: 12px;
-        padding: 8px 12px;
+        padding: 8px;
         border-radius: 8px;
         height: 100%;
         overflow-y: auto;
