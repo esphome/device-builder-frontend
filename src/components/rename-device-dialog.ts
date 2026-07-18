cd515459e49c36b1d3700be12c02ec40e051d94d
J. Nick Koston <nick@home-assistant.io>
2026-07-18 13:13:19 -1000

    Drop the dead nothing imports from the migrated dialogs

--- src/components/rename-device-dialog.ts
+++ src/components/rename-device-dialog.ts
@@ -1,5 +1,5 @@
 import { consume } from "@lit/context";
-import { LitElement, css, html, nothing } from "lit";
+import { LitElement, css, html } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import type { LocalizeFunc } from "../common/localize.js";
 import { localizeContext } from "../context/index.js";
